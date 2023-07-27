f6312214835709584cf55fd2a1830f4eeaf15442
Abbosbek <adev0197@gmail.com>
2023-07-27 14:42:32 +0500

    Retrofit Coroutines ViewModel MVVM With Room

---
+++ app/src/main/java/uz/abbosbek/mvvm_lesson_81/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.abbosbek.mvvm_lesson_81
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
